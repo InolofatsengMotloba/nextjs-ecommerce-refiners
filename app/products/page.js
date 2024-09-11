c642d166a24bc929d2da4417483435422cfbc143
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-11 16:56:07 +0200

    Create Products Page with grid layout and placeholder heading

---
+++ app/products/page.js
@@ -0,0 +1,7 @@
+export default function Products() {
+  return (
+    <div className="grid items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 min-h-screen">
+      <h1>Grid of Products</h1>
+    </div>
+  );
+}
