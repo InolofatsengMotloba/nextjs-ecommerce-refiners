a8a162e3014fcb2d2a5525cf197bde5ecb04e1c9
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-11 16:59:09 +0200

    Add placeholder paragraph tag to Home component with a description

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>Home Page</h1>
+      <p>This is where I'm gonna create a landing page</p>
     </div>
   );
 }
