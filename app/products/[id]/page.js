e7ba0ca2e9ddb74512b1f7b23f1f65ddeb1589ca
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-12 10:25:39 +0200

    Initialize ProductPage component with client-side product data fetching functionality & Added loading state and error handling

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,7 +1,47 @@
-export default function ProductDetails() {
+"use client";
+import { useState, useEffect } from "react";
+
+async function fetchProduct(id) {
+  const res = await fetch(
+    `https://next-ecommerce-api.vercel.app/products/${id}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch product");
+  }
+
+  return res.json();
+}
+
+export default function ProductDetails({ params }) {
+  const { id } = params;
+  const [product, setProduct] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadProduct() {
+      try {
+        const fetchedProduct = await fetchProduct(id);
+        setProduct(fetchedProduct);
+      } catch (err) {
+        setError("Failed to load product. Please try again later.");
+      }
+    }
+
+    loadProduct();
+  }, [id]);
+
+  if (error) {
+    return <p className="text-red-500">{error}</p>;
+  }
+
+  if (!product) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
-      <h1>Product Detailed Page</h1>
+      <p>{product.title}</p>
     </div>
   );
 }
