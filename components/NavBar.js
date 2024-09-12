a5a8d6b3a3382cc7c860163114d011369389bcf0
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-12 06:05:27 +0200

    Updated NavBar component with new logo, menu items, and login button

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,25 +1,48 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <header className="bg-[#9296938e] text-white py-4">
-      <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold">Logo goes here</h1>
-        <nav>
-          <ul className="flex space-x-4">
+    <header className="bg-[#92969357] text-white py-2">
+      <div className="container mx-auto px-4 flex items-center justify-between">
+        {/* Logo */}
+        <Link href="/">
+          <div className="flex items-center space-x-2">
+            <Image src="/Logo.png" alt="Logo" width={60} height={60} />
+            <h1 className="text-xl font-serif font-bold">Her Store</h1>
+          </div>
+        </Link>
+
+        {/* Menu Items */}
+        <nav className="flex-1">
+          <ul className="flex justify-center space-x-8">
             <li>
-              <Link href="/" className="hover:underline">
+              <Link href="/" className="text-lg hover:underline">
                 Home
               </Link>
             </li>
             <li>
-              <Link href="/products">Products</Link>
+              <Link href="/products" className="text-lg hover:underline">
+                Products
+              </Link>
             </li>
             <li>
-              <Link href="/cart">Cart</Link>
+              <Link href="/cart" className="text-lg hover:underline">
+                Cart
+              </Link>
             </li>
           </ul>
         </nav>
+
+        {/* Login Button */}
+        <div>
+          <Link
+            href="/login"
+            className="bg-gray-400 text-white py-2 px-4 rounded hover:bg-gray-500"
+          >
+            Login
+          </Link>
+        </div>
       </div>
     </header>
   );
