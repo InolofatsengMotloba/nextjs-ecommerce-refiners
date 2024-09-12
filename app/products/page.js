2ee1ecde04af303f89d058234c92237e4d3a4046
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-12 08:58:21 +0200

    Enable dynamic fetching for products page

--- app/products/page.js
+++ app/products/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"; // Ensure the page always fetches fresh data.
+
 export default function Products() {
   return (
     <div className="grid items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 min-h-screen">
