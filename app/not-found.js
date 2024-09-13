cb5b1c7b7836b9afcfbc4002ecf0b032ae50eb74
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-13 16:46:44 +0200

    Updated not-found page with new layout and design

--- app/not-found.js
+++ app/not-found.js
@@ -1,7 +1,44 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const BackButton = dynamic(() => import("@/components/BackButton"), {
+  ssr: false,
+});
+
 export default function NotFound() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1>404 Message</h1>
+    <div className="max-w-5xl mx-auto p-8">
+      <BackButton />
+      <div className="flex flex-col justify-center items-center mt-24">
+        <div className="text-center bg-gray-100 shadow-lg rounded-lg p-8 max-w-md mx-auto">
+          <svg
+            className="w-16 h-16 mx-auto mb-4 text-red-500"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="12"
+              cy="12"
+              r="11"
+              stroke="currentColor"
+              strokeWidth="2"
+              fill="none"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 8v4m0 4h.01"
+            />
+          </svg>
+
+          <h1 className="text-2xl font-bold text-gray-800 mb-2">404!</h1>
+          <p className="text-gray-600 mb-6">This page ccould not be found</p>
+        </div>
+      </div>
     </div>
   );
 }
