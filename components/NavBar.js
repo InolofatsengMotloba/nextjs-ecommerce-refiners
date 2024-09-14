33853bdb8e1d8f64e7935cd52358cb18ed3263f0
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-14 07:41:05 +0200

    Added mobile navigation menu with hamburger button and dropdown, refactored navigation items and login button

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function NavBar() {
+  const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarVisible(!isNavbarVisible);
+  };
+
   return (
-    <header className="bg-black text-white py-2">
+    <header className="bg-black text-white py-2 sticky top-0 z-50">
       <div className="container mx-auto px-4 flex items-center justify-between">
         {/* Logo */}
         <Link href="/">
@@ -15,9 +24,55 @@ export default function NavBar() {
           </div>
         </Link>
 
-        {/* Menu Items */}
-        <nav className="flex-1">
-          <ul className="flex justify-center space-x-8">
+        {/* Hamburger Menu Button */}
+        <button
+          onClick={toggleNavbar}
+          className="md:hidden text-white focus:outline-none"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+
+        {/* Larger Screens Menu */}
+        <nav className="hidden md:flex flex-1 justify-center space-x-8">
+          <Link href="/" className="text-lg hover:underline">
+            Home
+          </Link>
+          <Link href="/products" className="text-lg hover:underline">
+            Products
+          </Link>
+          <Link href="/cart" className="text-lg hover:underline">
+            Cart
+          </Link>
+        </nav>
+
+        {/* Login Button */}
+        <div className="hidden md:block">
+          <Link
+            href="/login"
+            className="text-white py-2 px-4 rounded hover:bg-gray-500"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+
+      {/* Mobile Dropdown Menu */}
+      {isNavbarVisible && (
+        <div className="md:hidden bg-black">
+          <ul className="flex flex-col items-center space-y-4 py-4">
             <li>
               <Link href="/" className="text-lg hover:underline">
                 Home
@@ -33,19 +88,14 @@ export default function NavBar() {
                 Cart
               </Link>
             </li>
+            <li>
+              <Link href="/login" className="text-lg hover:underline">
+                Login
+              </Link>
+            </li>
           </ul>
-        </nav>
-
-        {/* Login Button */}
-        <div>
-          <Link
-            href="/login"
-            className="text-white py-2 px-4 rounded hover:bg-gray-500"
-          >
-            Login
-          </Link>
         </div>
-      </div>
+      )}
     </header>
   );
 }
