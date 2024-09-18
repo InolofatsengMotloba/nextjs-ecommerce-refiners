bc52ca918145c0c0e80aeaf8d4a3c60b77e84239
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-18 15:01:56 +0200

    Remove loading state handling in ProductDetails component

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -42,10 +42,6 @@ export default async function ProductDetails({ params }) {
     throw error;
   }
 
-  if (!product) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <div className="max-w-5xl mx-auto p-8">
       <BackButton />
