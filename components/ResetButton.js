b3dea37985ad5768fb724c1a55e4fc8b97c6c87f
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-09-26 14:13:35 +0200

    Updated ResetButton.js: format code

--- components/ResetButton.js
+++ components/ResetButton.js
@@ -1,7 +1,7 @@
 export default function ResetButton() {
-    return (
-        <div className="mb-4">
-            <h1>Reset button</h1>
-        </div>
-    )
+  return (
+    <div className="mb-4">
+      <h1>Reset button</h1>
+    </div>
+  );
 }
